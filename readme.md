3c191dd09b056d818771c4a0723d3d383c9b1552
OrangeBird <yuanxinbj@gmail.com>
2024-04-12 23:45:20 +0800

    main

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 The new Journey to enhance my Frontend Skills!
 This is my own modification version of Lama's work.
 There might be some parody stuffs.
+
+1rem = 4 tailwindcss unit = 16px
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { DM_Sans, Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 // const inter = Inter({ subsets: ["latin"] });
 const font = DM_Sans({ subsets: ["latin"] });
@@ -12,7 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
+          <div className="h-24">
+            <Navbar />
+          </div>
+          <div className="h-[calc(100vh - 6rem)]">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
