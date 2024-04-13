ee15efbe8c7f8bc9c786354284ca16e940622f1e
OrangeBird <yuanxinbj@gmail.com>
2024-04-14 06:08:14 +0800

    main

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -58,6 +58,17 @@ const Navbar = () => {
     },
   };
 
+  const listItemVariants = {
+    closed: {
+      x: -10,
+      opacity: 0,
+    },
+    opened: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px:48 text-xl">
       {/* Links */}
@@ -113,7 +124,36 @@ const Navbar = () => {
             animate={open ? "opened" : "closed"}
             className="w-10 h-1 bg-black rounded origin-left"
           ></motion.div>
+          <motion.div
+            variants={centerVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded"
+          ></motion.div>
+          <motion.div
+            variants={bottomVariants}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
+          ></motion.div>
         </button>
+        {/* Menu List */}
+        {open && (
+          <motion.div
+            variants={listVariants}
+            className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40"
+            initial="closed"
+            animate="opened"
+          >
+            {links.map((link) => (
+              <motion.div
+                variants={listItemVariants}
+                className=""
+                key={link.title}
+              >
+                <Link href={link.url}>{link.title}</Link>
+              </motion.div>
+            ))}
+          </motion.div>
+        )}
       </div>
     </div>
   );
